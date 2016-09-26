478ab0cc2426668c05eac4ef06d94acff18e0e2d
Daniil Yurov <Daniil_Yurov@epam.com>
2016-09-26 18:58:12 +0300

    Initial commit

---
+++ src/main/java/com/dy/pos/Pos.java
@@ -0,0 +1,134 @@
+package com.dy.pos;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Pos {
+
+    private int deposit;
+    private List<Integer> supportedCoins;
+    private Map<String,Integer> priceList;
+    private Map<String, Integer> order;
+    private NavigableMap<Integer, Integer> coins;
+
+    public Pos() {
+        this.supportedCoins = formSupportedCoins();
+        this.order = formBlankOrder();
+        this.priceList = formPriceList();
+        this.coins = formCoins(this.supportedCoins);
+    }
+
+    private List<Integer> formSupportedCoins() {
+        return Arrays.asList(1, 5, 10, 20, 25, 50);
+    }
+
+    private Map<String, Integer> formBlankOrder() {
+        return new HashMap<>();
+    }
+
+    private NavigableMap<Integer, Integer> formCoins(List<Integer> supportedCoins) {
+        NavigableMap<Integer, Integer> coins = new TreeMap<>(Comparator.reverseOrder());
+        supportedCoins.forEach(coin -> coins.put(coin, 10));
+        return coins;
+    }
+
+    private Map<String,Integer> formPriceList() {
+        Map<String,Integer> priceList = new HashMap<>();
+        priceList.put("Tea", 15);
+        priceList.put("Coffee", 25);
+        priceList.put("Juice", 35);
+        return priceList;
+    }
+
+    private void insertCoins(List<Integer> change) {
+        change.forEach(this::insert);
+    }
+
+    private boolean areEqual(int deposit, List<Integer> change) {
+        return change.stream().reduce(0, (a, b) -> a + b).equals(deposit);
+    }
+
+    private List<Integer> withdrawCoins(int amount) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer value : coins.navigableKeySet()) {
+            int count = amount / value;
+            if (count > 0) {
+                int nc = coins.get(value);
+                int subtract = (nc - count >= 0) ? count : nc;
+                amount -= subtract * value;
+                coins.merge(value, subtract, (previous, current) -> previous - current);
+                for (int i = 0; i < subtract; i++) {
+                    result.add(value);
+                }
+            }
+        }
+        return result;
+    }
+
+    public void insert(int amount) {
+        if (supportedCoins.contains(amount)) {
+            coins.merge(amount, 1, (previous, current) -> previous + current);
+            deposit += amount;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getBalance() {
+        return deposit;
+    }
+
+    public Map<String,Integer> getPriceList() {
+        return priceList;
+    }
+
+    public List<Integer> getSupportedCoins() {
+        return supportedCoins;
+    }
+
+    public void addOrder(String productName, int quantity) {
+        if (priceList.containsKey(productName)) {
+            order.merge(productName, quantity, (previous, current) -> previous + current);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Map<String,Integer> buy() {
+        Map<String, Integer> purchase = new HashMap<>(order);
+        AtomicInteger amount = new AtomicInteger();
+        purchase.forEach((product, quantity) -> {
+            int price = priceList.get(product);
+            amount.addAndGet(price * quantity);
+        });
+        if (amount.get() > deposit) throw new IllegalStateException();
+        deposit -= amount.get();
+        order.clear();
+        return purchase;
+    }
+
+    public boolean isChangeAvailable() {
+        int amount = deposit;
+        for (Integer value : coins.navigableKeySet()) {
+            int count = amount / value;
+            if (count > 0) {
+                int nc = coins.get(value);
+                int subtract = (nc - count >= 0) ? count : nc;
+                amount -= subtract * value;
+            }
+        }
+        return amount == 0;
+    }
+
+    public List<Integer> getChange() {
+        if (deposit == 0) return Collections.emptyList();
+        List<Integer> change = withdrawCoins(deposit);
+        if (areEqual(deposit, change)) {
+            deposit = 0;
+            return change;
+        } else {
+            insertCoins(change);
+            throw new IllegalStateException();
+        }
+    }
+}
---
+++ src/test/java/com/dy/pos/PosTest.java
@@ -0,0 +1,136 @@
+package com.dy.pos;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.*;
+
+@RunWith(Theories.class)
+public class PosTest {
+
+    private Pos pos;
+
+    private static Map<String,Integer> getPriceList() {
+        Map<String, Integer> priceList = new HashMap<>();
+        priceList.put("Tea", 15);
+        priceList.put("Coffee", 25);
+        priceList.put("Juice", 35);
+        return priceList;
+    }
+
+    private static List<Integer> getSupportedCoins() {
+        return Arrays.asList(1, 5, 10, 20, 25, 50);
+    }
+
+    private int sumOf(List<Integer> returnedCoins) {
+        return returnedCoins.stream().reduce(0, (a, b) -> a + b);
+    }
+
+    @Before
+    public void setup() {
+        pos = new Pos();
+    }
+
+    @DataPoints("Coin values")
+    public static List<Integer> getCoinValues() {
+        return getSupportedCoins();
+    }
+
+    @DataPoints("Product names")
+    public static Set<String> getProductNames() {
+        return getPriceList().keySet();
+    }
+
+    @Theory
+    public void canInsertValidCoinValues(@FromDataPoints("Coin values") int coinValue) throws Exception {
+        pos.insert(coinValue);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertInvalidAmount_throwsIllegalArgumentException() throws Exception {
+        int invalid_amount = 26;
+        pos.insert(invalid_amount);
+    }
+
+    @Test
+    public void returnsValidBalance() throws Exception {
+        pos.insert(1);
+        pos.insert(5);
+        pos.insert(10);
+        assertThat(pos.getBalance(), is(16));
+    }
+
+    @Test
+    public void returnsPriceList() throws Exception {
+        Map<String, Integer> expectedPriceList = getPriceList();
+        Map<String, Integer> actualPriceList = pos.getPriceList();
+        assertThat(actualPriceList.entrySet(), is(expectedPriceList.entrySet()));
+    }
+
+    @Test
+    public void returnsSupportedCoins() throws Exception {
+        List<Integer> expectedSupportedCoins = getSupportedCoins();
+        List<Integer> actualSupportedCoins = pos.getSupportedCoins();
+        assertThat(actualSupportedCoins, is(expectedSupportedCoins));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addInvalidProductName_throwsIllegalArgumentException() throws Exception {
+        pos.addOrder("UnsupportedName", 2);
+    }
+
+    @Theory
+    public void canBuy(@FromDataPoints("Product names") String productName) throws Exception {
+        Map<String, Integer> expectedBucket = new HashMap<>();
+        expectedBucket.put(productName, 1);
+        pos.addOrder(productName, 1);
+        pos.insert(50);
+        Map<String, Integer> actualBucket = pos.buy();
+        assertThat(actualBucket, is(expectedBucket));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void buyWithInsufficientFunds_throwsIllegalStateException() throws Exception {
+        pos.addOrder("Tea", 1);
+        pos.buy();
+    }
+
+    @Test
+    public void canGetChangeIfNothingPurchased() throws Exception {
+        List<Integer> insertedCoins = new ArrayList<>();
+        insertedCoins.add(5);
+        insertedCoins.add(10);
+        insertedCoins.forEach(pos::insert);
+        List<Integer> returnedCoins = pos.getChange();
+        assertThat(returnedCoins.size(), is(2));
+        assertTrue(returnedCoins.containsAll(insertedCoins));
+    }
+
+    @Test
+    public void returnsCorrectChangeAfterPurchase() throws Exception {
+        pos.insert(50);
+        pos.addOrder("Tea", 1);
+        pos.buy();
+        List<Integer> returnedCoins = pos.getChange();
+        assertThat(sumOf(returnedCoins), is(35));
+    }
+
+    @Test
+    public void returnsTrueIfChangeIsAvailable() throws Exception {
+        pos.insert(10);
+        assertThat(pos.isChangeAvailable(), is(true));
+    }
+
+    @Test
+    public void returnsFalseIfChangeIsUnavailable() throws Exception {
+
+
+    }
+}
